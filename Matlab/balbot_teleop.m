d5db107f79136d35f2ee3bf676e1c0e6523573d2
Dan <doates625@gmail.com>
2019-12-16 20:36:08 -0500

    Variable name refactor

--- Matlab/balbot_teleop.m
+++ Matlab/balbot_teleop.m
@@ -1,33 +1,42 @@
-function balbot_teleop(name, ctrl_type, vel_max, yaw_max)
+function balbot_teleop(...
+    bot_name, ...
+    ctrl_type, ...
+    lin_vel_max, ...
+    lin_acc_max, ...
+    yaw_vel_max)
 %BALBOT_TELEOP Runs bluetooth teleoperation of self-balancing robot.
-%   name = Bluetooth device name [ex. 'ES3011_BOT01']
-%   ctrl_type = Controller type ['Nintendo64', 'Xbox360']
-%   vel_max = Max linear velocity command [m/s]
-%   yaw_max = Max yaw velocity command [rad/s]
+%   bot_name = Bluetooth device name [ex. 'ES3011_BOT01']
+%   ctrl_type = Controller type ['Xbox360', 'Nintendo64']
+%   lin_vel_max = Max linear velocity command [m/s]
+%   lin_acc_max = Max linear acceleration command [m/s^2]
+%   yaw_vel_max = Max yaw velocity command [rad/s]
 
 % Default arguments
 if nargin < 1
-    name = 'BalBot';
+    bot_name = 'BalBot';
 end
 if nargin < 2
     ctrl_type = 'Xbox360';
 end
 if nargin < 3
-    vel_max = 0.8;
+    lin_vel_max = 0.8;
 end
 if nargin < 4
-    yaw_max = 1.6;
+    lin_acc_max = 0.8;
+end
+if nargin < 5
+    yaw_vel_max = 1.6;
 end
 
 % Create game controller interface
 switch ctrl_type
-    case 'Nintendo64'
-        joy_dz = 0.04;  % Joystick deadzone
-        ctrl = Nintendo64(1, joy_dz);
     case 'Xbox360'
-        joy_dz = 0.08;  % Joystick deadzone
-        trig_dz = 0.01; % Trigger deadzone
+        joy_dz = 0.08;
+        trig_dz = 0.01;
         ctrl = Xbox360(1, joy_dz, trig_dz);
+    case 'Nintendo64'
+        joy_dz = 0.04;
+        ctrl = Nintendo64(1, joy_dz);
     otherwise
         error('Invalid controller type')
 end
@@ -35,16 +44,16 @@ end
 % Connect to robot
 clc
 fprintf('Balancing Robot Controller\n')
-fprintf(['Connecting to robot ''' name ''' ...\n'])
-balbot = BalBot(name, vel_max, yaw_max);
+fprintf(['Connecting to robot ''' bot_name ''' ...\n'])
+balbot = BalBot(bot_name, lin_vel_max, yaw_vel_max);
 balbot.connect();
 pause(2)
 
 % Log vectors
 log_n = 10000;
-lin_cmd = zeros(log_n, 1);
+lin_vel_cmd = zeros(log_n, 1);
+yaw_vel_cmd = zeros(log_n, 1);
 lin_vel = zeros(log_n, 1);
-yaw_cmd = zeros(log_n, 1);
 yaw_vel = zeros(log_n, 1);
 volts_L = zeros(log_n, 1);
 volts_R = zeros(log_n, 1);
@@ -59,24 +68,24 @@ while 1
     % Get commands from game controller
     switch ctrl_type
         case 'Xbox360'
-            lin_cmd(log_i) = vel_max * ctrl.trig();
-            yaw_cmd(log_i) = -yaw_max * ctrl.joy_Lx();
+            lin_vel_cmd(log_i) = lin_vel_max * ctrl.trig();
+            yaw_vel_cmd(log_i) = -yaw_vel_max * ctrl.joy_Lx();
         case 'Nintendo64'
-            lin_cmd(log_i) = vel_max * ctrl.joy_y();
-            yaw_cmd(log_i) = -yaw_max * ctrl.joy_x();
+            lin_vel_cmd(log_i) = lin_vel_max * ctrl.joy_y();
+            yaw_vel_cmd(log_i) = -yaw_vel_max * ctrl.joy_x();
     end
     
     try
         % Send commands to robot and get state
-        state = balbot.send_cmds(lin_cmd(log_i), yaw_cmd(log_i));
+        state = balbot.send_cmds(lin_vel_cmd(log_i), yaw_vel_cmd(log_i));
         
         % Display state
         clc
         fprintf('Balancing Robot Controller\n')
         fprintf('Press B to exit.\n\n')
-        fprintf('Vel cmd: %+.2f [m/s]\n', lin_cmd(log_i))
+        fprintf('Lin vel cmd: %+.2f [m/s]\n', lin_vel_cmd(log_i))
         fprintf('Lin vel: %+.2f [m/s]\n', state.lin_vel)
-        fprintf('Yaw cmd: %+.2f [rad/s]\n', yaw_cmd(log_i))
+        fprintf('Yaw vel cmd: %+.2f [rad/s]\n', yaw_vel_cmd(log_i))
         fprintf('Yaw vel: %+.2f [rad/s]\n', state.yaw_vel)
         fprintf('Voltage L: %+.1f [V]\n', state.volts_L)
         fprintf('Voltage R: %+.1f [V]\n', state.volts_R)
@@ -96,9 +105,9 @@ while 1
     % Exit conditions
     exit_btn = ctrl.btn_B();
     if exit_btn || com_error
-        lin_cmd = lin_cmd(1:log_i);
+        lin_vel_cmd = lin_vel_cmd(1:log_i);
         lin_vel = lin_vel(1:log_i);
-        yaw_cmd = yaw_cmd(1:log_i);
+        yaw_vel_cmd = yaw_vel_cmd(1:log_i);
         yaw_vel = yaw_vel(1:log_i);
         volts_L = volts_L(1:log_i);
         volts_R = volts_R(1:log_i);
@@ -133,7 +142,7 @@ hold on, grid on
 title('Linear Velocity')
 xlabel('Time [s]')
 ylabel('Velocity [m/s]')
-plot(t, lin_cmd, 'k--')
+plot(t, lin_vel_cmd, 'k--')
 plot(t, lin_vel, 'b-')
 legend('Setpt', 'Value')
 
@@ -143,7 +152,7 @@ hold on, grid on
 title('Yaw Velocity')
 xlabel('Time [s]')
 ylabel('Velocity [rad/s]')
-plot(t, yaw_cmd, 'k--')
+plot(t, yaw_vel_cmd, 'k--')
 plot(t, yaw_vel, 'b-')
 legend('Setpt', 'Value')
 
