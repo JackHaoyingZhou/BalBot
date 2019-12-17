103a55a08e9bf0a61a7ff1a11057f242f22c596a
Dan <doates625@gmail.com>
2019-12-16 22:20:25 -0500

    Moved Slewlimiter to external libraries and fixed

--- Matlab/BalBot.m
+++ Matlab/BalBot.m
@@ -5,10 +5,9 @@ classdef BalBot < handle
     properties (Access = private)
         device_name;    % Bluetooth device name [String]
         serial_comms;   % Embedded comms interface [SerialComms]
-        lin_vel_max;    % Max linear velocity cmd [m/s]
-        lin_acc_max;    % Max linear acceleration cmd [m/s^2]
-        yaw_vel_max;    % Max yaw velocity cmd [rad/s]
-        acc_limiter;    % Command acceleration limiter [SlewLimiter]
+        lin_vel_lim;    % Linear velocity limiter [ClampLimiter]
+        lin_acc_lim;    % Linear acceleration limiter [SlewLimiter]
+        yaw_vel_lim;    % Yaw velocity limiter [ClampLimiter]
     end
     
     methods
@@ -17,19 +16,18 @@ classdef BalBot < handle
                 lin_vel_max, ...
                 lin_acc_max, ...
                 yaw_vel_max)
-            %BALBOT Construct robot interface.
+            %BALBOT Construct robot interface
             %   bot_name = Bluetooth name [ex. 'ES3011_BOT01']
             %   lin_vel_max = Max linear velocity command [m/s]
             %   lin_acc_max = Max linear acceleration command [m/s^2]
             %   yaw_vel_max = Max yaw velocity command [rad/s]
             obj.device_name = bot_name;
-            obj.lin_vel_max = lin_vel_max;
-            obj.lin_acc_max = lin_acc_max;
-            obj.yaw_vel_max = yaw_vel_max;
-            obj.acc_limiter = SlewLimiter(-lin_acc_max, lin_acc_max, 10.0); % Bullshit frequency - fix
+            obj.lin_vel_lim = ClampLimiter(lin_vel_max);
+            obj.lin_acc_lim = SlewLimiter(lin_acc_max);
+            obj.yaw_vel_lim = ClampLimiter(yaw_vel_max);
         end
         function connect(obj)
-            %CONNECT Resets bluetooth connection.
+            %CONNECT Resets bluetooth connection
             instrreset;
             bt_info = instrhwinfo('bluetooth');
             n = length(bt_info.RemoteNames);
@@ -47,30 +45,26 @@ classdef BalBot < handle
             obj.serial_comms.open();
         end
         function disconnect(obj)
-            %DISCONNECT Disconnects from bluetooth.
+            %DISCONNECT Disconnects from bluetooth
             obj.serial_comms.close();
         end
         function state = send_cmds(obj, lin_vel_cmd, yaw_vel_cmd)
-            %SEND_CMDS Sends commands and returns state struct.
-            %   lin_vel_cmd = Linear velocity cmd [m/s]
-            %   yaw_vel_cmd = Yaw velocity cmd [rad/s]
-            %   state.lin_vel_cmd = Limited linear velocity cmd [m/s]
-            %   state.yaw_vel_cmd = Limited yaw velocity cmd [rad/s]
-            %   state.lin_vel = Robot linear velocity [m/s]
-            %   state.yaw_vel = Robot yaw velocity [rad/s]
-            %   state.volts_L = Left motor voltage [V]
-            %   state.volts_R = Right motor voltage [V]
+            %SEND_CMDS Sends commands and returns state struct
+            %   Inputs:
+            %       lin_vel_cmd = Linear velocity [m/s]
+            %       yaw_vel_cmd = Yaw velocity [rad/s]
+            %   Outputs:
+            %       state.lin_vel_cmd = Limited linear velocity [m/s]
+            %       state.yaw_vel_cmd = Limited yaw velocity [rad/s]
+            %       state.lin_vel = Robot linear velocity [m/s]
+            %       state.yaw_vel = Robot yaw velocity [rad/s]
+            %       state.volts_L = Left motor voltage [V]
+            %       state.volts_R = Right motor voltage [V]
             
             % Filter input commands
-            lin_vel_cmd = clamp_limit(...
-                lin_vel_cmd, ...
-                -obj.lin_vel_max, ...
-                +obj.lin_vel_max);
-            lin_vel_cmd = obj.acc_limiter.update(lin_vel_cmd);
-            yaw_vel_cmd = clamp_limit(...
-                yaw_vel_cmd, ...
-                -obj.yaw_vel_max, ...
-                +obj.yaw_vel_max);
+            lin_vel_cmd = obj.lin_vel_lim.update(lin_vel_cmd);
+            lin_vel_cmd = obj.lin_acc_lim.update(lin_vel_cmd);
+            yaw_vel_cmd = obj.yaw_vel_lim.update(yaw_vel_cmd);
             
             % Send filtered commands
             obj.serial_comms.write_float(lin_vel_cmd);
